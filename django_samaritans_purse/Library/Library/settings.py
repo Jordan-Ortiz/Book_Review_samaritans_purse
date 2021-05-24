21dd59f05534a3c7224a88699bb525ea3af4b2fb
Jordan-Ortiz <84547534+Jordan-Ortiz@users.noreply.github.com>
2021-05-24 10:14:03 -0500

    add 127.0.0.1

--- django_samaritans_purse/Library/Library/settings.py
+++ django_samaritans_purse/Library/Library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jrwp7=i(4yp)ug1d%acw!@04m2l+gh%=zra_4a!3%wct*%%pd)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
